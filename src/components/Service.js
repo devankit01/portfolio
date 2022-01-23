75cf07d59db599251513228e8f74db6548cec60c
Ankit Gupta <58206597+devankit01@users.noreply.github.com>
2022-01-23 15:10:45 +0530

    Update Service.js

--- src/components/Service.js
+++ src/components/Service.js
@@ -10,18 +10,18 @@ function Service() {
                 <div className="card">
                         <img src="https://bolby-react.vercel.app/images/service-1.svg" alt="" />
                         <h3>Web Design</h3>
-                        <p>lorem ipsum dolor sit amet, consectetur adip </p>
+                    
                 </div>
                 <div className="card">
                 <img src="https://bolby-react.vercel.app/images/service-2.svg" alt="" />
                         <h3>Web Development</h3>
-                        <p>lorem ipsum dolor sit amet, consectetur adip </p>
+                    
                 </div>
         
                 <div className="card">
                 <img src="https://bolby-react.vercel.app/images/price-1.svg" alt="" />
                         <h3>SEO and Digital</h3>
-                        <p>lorem ipsum dolor sit amet, consectetur adip </p>
+                  
                
                 </div>
                 </div>
